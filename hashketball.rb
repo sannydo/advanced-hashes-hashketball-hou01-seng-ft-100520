3d7205da2484c76eb87de14109e944d021559cff
Sanny Do <sanny.do94@gmail.com>
2020-08-31 22:15:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,7 +194,7 @@ end
 def player_numbers(team_name, game)
   game.each do |team, game_hash|
     if game_hash[:name] == team_name
-      return (game_hash)
+      return (game_hash[:players])
   end
 end
 
