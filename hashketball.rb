26d0e45b508abd6e325c0721c1ae7721a41891f4
Sanny Do <sanny.do94@gmail.com>
2020-08-31 22:13:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,7 +189,7 @@ end
 
 # * Build a method, `player_numbers`, that takes in an argument of a team name and
 #   returns an `Array` of the jersey numbers for that team.
-p team_name
+
 
 def player_numbers(team_names)
   team(team_name)[:players].map do |key, values|
